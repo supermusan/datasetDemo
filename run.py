16c08ec86fe12dd9bf84402617af5cb4f1bc4c64
supermusan <musan1212@qq.com>
2021-02-09 10:13:36 +0800

    10

--- run.py
+++ run.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 with open("pkls/datadict_vox2.pkl", "rb") as fp:
     datadict = pickle.load(fp)
 
+exp_name= [list(datadict.keys())[idx] for idx in range(0,50000,5000)]
+exp_img = [datadict[_name]["frame"][0] for _name in exp_name]
+expdict=[]
+for _name, _img in zip(exp_name, exp_img):
+    expdict.append({"name":_name, "img":_img})
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/search', methods=['post'])
+@app.route('/search', methods=['GET'])
+@app.route('/voxceleb2/search', methods=['GET'])
 def search():
-    name = request.form.get('name')
+    name = request.args.get('name')
     if name not in datadict.keys():
         return redirect(url_for('voxceleb2'))
     else:
@@ -20,15 +27,18 @@ def search():
 
 
 @app.route('/voxceleb2')
+@app.route('/voxceleb2/')
 @app.route('/voxceleb2/<string:videoname>')
 def voxceleb2(videoname=None):
     if videoname==None or videoname not in datadict.keys():
-        return render_template('vox2.html', all_videonames=list(datadict.keys()), 
+        return render_template('vox2.html', all_videonames=list(datadict.keys()),
+                            exp=expdict,
                             videoname=videoname,
                             videopath=None, 
                             framelist=[])
     else:
-        return render_template('vox2.html', all_videonames=list(datadict.keys()), 
+        return render_template('vox2.html', all_videonames=list(datadict.keys()),
+                            exp=[],
                             videoname=videoname,
                             videopath=datadict[videoname]["video"], 
                             framelist=datadict[videoname]["frame"])
